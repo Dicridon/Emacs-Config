7be647934ddab7e1fbfb5628f2de9461fd8bd17d
Dicridon <noahxiong@outlook.com>
2021-04-01 16:34:51 +0800

    flycheck now recognizes project root

--- lisp/init-customization.el
+++ lisp/init-customization.el
@@ -36,18 +36,18 @@
 ;;                   :foreground "#FF9108"))))))
 ;; FF9108, CD770A
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
- '(company-scrollbar-bg ((t (:background "#488848884888"))))
- '(company-scrollbar-fg ((t (:background "#3bbb3bbb3bbb"))))
- '(company-tooltip ((t (:inherit default :background "#3bbb3bbb3bbb"))))
- '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "#FF9108"))))
- '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- '(company-tooltip-selection ((t (:background "#622162216221")))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:background nil))))
+;;  '(company-scrollbar-bg ((t (:background "#488848884888"))))
+;;  '(company-scrollbar-fg ((t (:background "#3bbb3bbb3bbb"))))
+;;  '(company-tooltip ((t (:inherit default :background "#3bbb3bbb3bbb"))))
+;;  '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "#FF9108"))))
+;;  '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+;;  '(company-tooltip-selection ((t (:background "#622162216221")))))
 
 ;; No more yes or no
 (fset 'yes-or-no-p 'y-or-n-p)
--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,13 +2,27 @@
 ;;; Commentary:
 ;;; Code:
 
+
+(require 'projectile)
+(add-hook 'c++-mode-hook 'setup-flycheck-clang-project-path)
 (use-package flycheck
   :ensure t
   :config
   (global-flycheck-mode)
-  (setq flycheck-clang-include-path
-        (list "./src/" "./src/components/" "./components/"))
-  (setq flycheck-idle-change-delay 0.5))
+  (setq flycheck-idle-change-delay 0.5)
+  (setq flycheck-clang-language-standard "c++17"))
+
+(defun dicridon-setup-flycheck-clang-project-path-canoe ()
+  "Prepare project root for files in canoe project."
+  (let ((root (ignore-errors (projectile-project-root ".")))
+        (dirs '("src" "src/components")))
+    (when root
+      (let ((final (mapcar (lambda(c) (concat root c)) dirs)))
+        (dolist (dir final)
+          (add-to-list 'flycheck-clang-include-path dir))))))
+
+(add-hook 'c++-mode-hook 'dicridon-setup-flycheck-clang-project-path-canoe)
+
 
 (provide 'init-flycheck)
 ;;; init-flycheck ends here
