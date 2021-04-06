cb22c990f96937cd3b6547c9a163b943e2d11a50
Dicridon <noahxiong@outlook.com>
2021-04-06 20:23:38 +0800

    latex utility added

---
+++ lisp/init-latex.el
@@ -0,0 +1,14 @@
+;;; init-latex --- Summary
+;;; Commentary:
+;;; Code:
+(use-package latex
+  :ensure auctex
+  :config
+  (add-hook 'LaTeX-mode-hook (lambda ()
+                               (let ((xelatexmk '("xelatexmk" "latexmk -xelatex main.tex"
+                                                  TeX-run-TeX nil t
+                                                  :help "Run latexmk on file")))
+                                 (unless (member xelatexmk TeX-command-list)
+                                   (push  xelatexmk TeX-command-list))))))
+(provide 'init-latex)
+;;; init-latex ends here
