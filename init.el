1233da0889bc686d7ce1d31c6c3075bccb1c5d4d
Dicridon <noahxiong@outlook.com>
2021-04-09 14:06:09 +0800

    paredit mode for emacs lisp

--- init.el
+++ init.el
@@ -12,12 +12,10 @@
  '(custom-safe-themes
    '("b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" default))
  '(package-selected-packages
-   '(latex-preview-pane auctex paredit company-fuzzy treemacs-projectile linum-relative flycheck-rust window-numbering eglot-jl enh-ruby-mode robe zenity-color-picker sublime-themes dumb-jump treemacs ivy projectile switch-window smex go-mode rust-mode scala-mode switch-window- ace-window neotree speedbar-git-respect company-c-headers company flycheck))
+   '(markdown-mode company-auctex latex-preview-pane auctex paredit company-fuzzy treemacs-projectile linum-relative flycheck-rust window-numbering eglot-jl enh-ruby-mode robe zenity-color-picker sublime-themes dumb-jump treemacs ivy projectile switch-window smex go-mode rust-mode scala-mode switch-window- ace-window neotree speedbar-git-respect company-c-headers company flycheck))
  '(safe-local-variable-values
    '((company-c-headers-path-user "./src/components/" "./components/")
      (company-clang-arguments "-I./src/components/" "-I./components/"))))
-
-
 
 (require 'init-use-package)
 (require 'init-projectile)
@@ -29,7 +27,6 @@
 (require 'init-smex)
 (require 'init-avy)
 (require 'init-switch-window)
-
 
 (require 'init-ivy)
 (require 'init-avy)
@@ -46,6 +43,7 @@
 (require 'init-ruby)
 (require 'init-rust)
 (require 'init-go)
+(require 'init-elisp)
 
 ;; customizations
 (require 'init-customization)
---
+++ lisp/init-elisp.el
@@ -0,0 +1,6 @@
+;;; init-elisp -- Summary
+;;; Commentary:
+;;; Code:
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(provide 'init-elisp)
+;;; init-elisp ends here
