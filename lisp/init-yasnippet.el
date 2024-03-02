f1d6a8439139e855bd54be2dc1b6d682c641fa4b
Dicridon <noahxiong@outlook.com>
2024-03-02 15:49:57 +0800

    adding some C++-14 style snippets

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,7 +1,11 @@
 ;;; init --- Summary
 ;;; Commentary:
 ;;; Code:
-(require 'yasnippet)
+(use-package 'yasnippet
+  :ensure t)
+(use-package 'yasnippet-snippets
+  :ensure t)
+
 (yas-global-mode 1)
 
 (provide 'init-yasnippet)
