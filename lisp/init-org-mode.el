7cf00aaab696bea41b8444710ae2c462922dfbe4
Dicridon <noahxiong@outlook.com>
2024-03-11 20:00:42 +0800

    some personal org functions for TODO list added

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -6,16 +6,53 @@
  'org-babel-load-languages
  '((dot . t)))
 (setq org-confirm-babel-evaluate nil)
-(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-todo-keywords '((sequence "TODO(t)" "ONGOING(o)" "|" "DONE(d)" "CANCELLED(c)")))
 (setq org-agenda-files (list "~/.emacs.d/Schedule/timetable.org"))
 
 (defun org-summary-todo (n-done n-not-done)
   "Switch entry to DONE when all subentries are done, to TODO otherwise."
   (let (org-log-done org-log-states)   ; turn off logging
     (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
 (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+;; snippet below is from
+;; https://emacs.stackexchange.com/questions/19843/how-to-automatically-adjust-an-org-task-state-with-its-children-checkboxes
+(defun d/org-checkbox-todo ()
+  "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+  (let ((todo-state (org-get-todo-state)) beg end)
+    (unless (not todo-state)
+      (save-excursion
+        (org-back-to-heading t)
+        (setq beg (point))
+        (end-of-line)
+        (setq end (point))
+        (goto-char beg)
+        (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+                               end t)
+            (if (match-end 1)
+                (if (equal (match-string 1) "100%")
+                    (unless (string-equal todo-state "DONE")
+                      (org-todo 'done))
+                  (if (equal (match-string 1) "0%")
+                      (unless (string-equal todo-state "TODO")
+                        (org-todo 'todo))
+                    (unless (string-equal todo-state "ONGOING")
+                      (org-todo "ONGOING"))))
+              (if (and (> (match-end 2) (match-beginning 2)) ;; ensure cookies are activated
+                       (equal (match-string 2) (match-string 3)))
+                  (unless (string-equal todo-state "DONE")
+                    (org-todo 'done))
+                (if (equal (match-string 2) "0")
+                    (unless (string-equal todo-state "TODO")
+                      (org-todo 'todo))
+                  (unless (string-equal todo-state "ONGOING")
+                    (org-todo "ONGOING"))))))))))
+
+(add-hook 'org-checkbox-statistics-hook 'd/org-checkbox-todo)
 
 (global-set-key "\C-ca" 'org-agenda)
 
 (provide 'init-org-mode)
 ;;; init-org-mode ends here
+
