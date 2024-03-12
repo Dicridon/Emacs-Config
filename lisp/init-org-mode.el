15cfa463b0005b00caa16bab6bc17195291dedbe
Dicridon <noahxiong@outlook.com>
2024-03-12 20:56:09 +0800

    Enable both cooies of TODO and checkboxes to propagate to parent entries

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -9,12 +9,26 @@
 (setq org-todo-keywords '((sequence "TODO(t)" "ONGOING(o)" "|" "DONE(d)" "CANCELLED(c)")))
 (setq org-agenda-files (list "~/.emacs.d/Schedule/timetable.org"))
 
-(defun org-summary-todo (n-done n-not-done)
-  "Switch entry to DONE when all subentries are done, to TODO otherwise."
-  (let (org-log-done org-log-states)   ; turn off logging
-    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(defun d/org-summary-todo (n-done n-not-done)
+  "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+  (let ((todo-state (org-get-todo-state)) beg end)
+    (unless (not todo-state)
+      (save-excursion
+        (org-back-to-heading t)
+        (setq beg (point))
+        (end-of-line)
+        (setq end (point))
+        (goto-char beg)
+        (if (= n-not-done 0)
+            (unless (string-equal todo-state "DONE")
+              (org-todo 'done))
+          (if (= n-done 0)
+              (unless (string-equal todo-state "TODO")
+                (org-todo 'todo))
+            (unless (string-equal todo-state "ONGOING")
+              (org-todo "ONGOING"))))))))
 
-(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+(add-hook 'org-after-todo-statistics-hook 'd/org-summary-todo)
 
 ;; snippet below is from
 ;; https://emacs.stackexchange.com/questions/19843/how-to-automatically-adjust-an-org-task-state-with-its-children-checkboxes
